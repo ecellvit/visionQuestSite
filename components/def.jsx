75fa491a67797a1fa897650cffb4b415bb3f0445
Pratyush <connect2pratyush@gmail.com>
2023-09-08 23:41:16 +0530

    asdf

---
+++ components/def.jsx
@@ -0,0 +1,7 @@
+export default function Def() {
+  return (
+    <div>
+      <h1>This is the DEF component</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Abc from '@/components/abc'
+import Def from '@/components/def'
+import { useEffect, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const pages = [
+    <Abc />,
+    <Def />
+  ]
+
+  const [currentPage, setCurrentPage] = useState(pages[0])
+  const [i, setI] = useState(0)
+
+  useEffect(()=>{
+    setCurrentPage(pages[i])
+  }, [i])
+
+  function NextButtonClick() {
+    // going to the next page
+    setI(i + 1)
+  }
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       Vision Quest
+      {currentPage}
+      <button onClick={()=>{NextButtonClick()}}>Next</button>
     </main>
   )
 }
