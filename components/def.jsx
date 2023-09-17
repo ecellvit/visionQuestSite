464ae302446965aa50ca770d9820eb7ddf9347c0
Pratyush <connect2pratyush@gmail.com>
2023-09-17 19:27:52 +0530

    navigation

--- components/def.jsx
+++
@@ -1,7 +0,0 @@
-export default function Def() {
-  return (
-    <div>
-      <h1>This is the DEF component</h1>
-    </div>
-  )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,24 @@
 import Abc from '@/components/abc'
-import Def from '@/components/def'
 import Sectorentry from '@/components/Sectorentry'
 import { useEffect, useState } from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import TeamForm from '@/components/teamform'
+import Cities from '@/components/Cities'
 
 export default function Home() {
 
   const { data: session, status } = useSession()
-  const [cityName,setCityName]=useState("DELHI")
-  const [industryName,setIndustryName] = useState("FASHION")
+  const [cityName, setCityName] = useState("DELHI")
+  const [industryName, setIndustryName] = useState("FASHION")
 
   const [hasTeamDetails, setHasTeamDetails] = useState(true)
   const [currentRound, setCurrentRound] = useState("Round 1")
 
+  const [stage, setStage] = useState("cities")
+
   const teamName = "Asdf";
   const teamNumber = "1234";
   const leaderEmail = "abc@vitstudent.ac.in";
-
-  const pages = [
-    <Sectorentry cityName={cityName} industryName={industryName}/>,
-    <Abc />,
-    <Def />
-    
-  ]
-
-  const [currentPage, setCurrentPage] = useState(pages[0])
-  const [i, setI] = useState(0)
-
-  useEffect(() => {
-    setCurrentPage(pages[i])
-  }, [i])
-
-  function NextButtonClick() {
-    // going to the next page
-    setI(i + 1)
-  }
 
   return (
     <div>
@@ -45,20 +28,22 @@ export default function Home() {
             <div>
               <div id="Header">
 
-              {/* <p>Logged in, {session.user.name}</p> */}
-              Vision Quest
-              {/* {currentPage} */}
-              <span>TeamName: {teamName}</span>
-              <span>TeamNumber: {teamNumber}</span>
-              <span>LeaderEmail: {leaderEmail}</span>
-              <span>{currentRound}</span>
+                {/* <p>Logged in, {session.user.name}</p> */}
+                Vision Quest
+                {/* {currentPage} */}
+                <span>TeamName: {teamName}</span>
+                <span>TeamNumber: {teamNumber}</span>
+                <span>LeaderEmail: {leaderEmail}</span>
+                <span>{currentRound}</span>
               </div>
-              
+
               <div id="Content">
-              {currentPage}
-              <button onClick={() => signOut()}>Log Out</button>
+                {stage == "cities" && <Cities onProceed={() => { setStage("sectors") }} />}
+                {stage == "sectors" && <Sectorentry cityName={cityName} industryName={industryName} onProceed={()=>{setStage("abc")}} />}
+                {stage == "abc" && <Abc />}
+                
+                <button onClick={() => signOut()}>Log Out</button>
               </div>
-              {/* <button onClick={() => { NextButtonClick() }}>Next</button> */}
             </div>
             :
             <div>
