201e22dd2ad863fe74844a5b0da8458c2d80f037
kv8gh <vyaskaran1409@gmail.com>
2023-09-17 22:06:46 +0530

    css change

--- components/waiting.jsx
+++ components/waiting.jsx
@@ -1,7 +1,7 @@
 export default function Waiting(props){
     return(
-        <div>
-            <h1>Waiting for other teams to complete</h1>
+        <div className="waitingDiv">
+            <h1 className="waiting">Waiting for other teams to complete</h1>
             <button onClick={()=>props.onProceed()}>Proceed</button>    
         </div>
     )
