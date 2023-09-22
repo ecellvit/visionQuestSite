35c47d16c43055e703b9a411d49829ce399bdcc3
Pratyush <connect2pratyush@gmail.com>
2023-09-22 12:56:10 +0530

    disq prob

--- pages/index.js
+++ pages/index.js
@@ -94,7 +94,7 @@ export default function Home() {
               </div>
 
               <div id="Content" className={styles.mainContent}>
-                {!qualified ? <Waiting text="You have been disqualified" onProceed={() => { location.reload() }} />
+                {(qualified!==undefined && !qualified) ? <Waiting text="You have been disqualified" onProceed={() => { location.reload() }} />
                   :
                   <>
                     {/* {stage} */}
